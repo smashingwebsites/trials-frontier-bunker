bd3b19d1f540f81bebd68f08c3e5dd7e52db4ead
Alex <alex@smashing-websites.de>
2022-07-26 23:11:50 +0200

    added heroicons as components

--- components/NewStrength.vue
+++ components/NewStrength.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="relative text-white rounded" :class="playerStrengthColor">
-    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 absolute left-2 top-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
-      /></svg
-    >
+    <ScaleIcon class="w-4 h-4 absolute left-2 top-1.5"/>
     <select
       style="background-image: none;"
       class="relative py-1 pr-2 text-sm text-center bg-transparent border-none rounded outline-none pl-7"
@@ -20,6 +14,8 @@
   </div>
 </template>
 <script setup>
+import { ScaleIcon } from '@heroicons/vue/solid'
+
 const props = defineProps(["strength"]);
 const tagRating = [
   {value: 0, label: 'N/A'},
--- components/Rank.vue
+++ components/Rank.vue
@@ -1,18 +1,7 @@
 <template>
   <div class="relative rounded bg-amber-400">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="w-4 h-4 absolute left-2 top-1.5"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z"
-        clip-rule="evenodd"
-      />
-    </svg>
 
+  <TrendingUpIcon class="w-4 h-4 absolute left-2 top-1.5"/>
     <select
       style="background-image: none"
       class="relative py-1 pr-2 text-sm text-center bg-transparent border-none rounded outline-none pl-7"
@@ -26,5 +15,6 @@
   </div>
 </template>
 <script setup>
+import { TrendingUpIcon } from '@heroicons/vue/solid'
 const props = defineProps(["rank", "playerId"])
 </script>
